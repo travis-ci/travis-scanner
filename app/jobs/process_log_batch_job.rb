577bb3566e26e4cb446cf7914a7da6376ce31ab0
AndriiMysko <usandrewmisko@gmail.com>
2022-08-10 17:29:27 +0300

    Change queue name

--- app/jobs/process_log_batch_job.rb
+++ app/jobs/process_log_batch_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ProcessLogBatchJob < ApplicationJob
-  queue_as :default
+  queue_as :logs_for_scanning
 
   def perform(log_ids)
     ProcessLogs.new(log_ids).call
