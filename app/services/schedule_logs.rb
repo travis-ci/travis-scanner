3a60bcda474f5925edf0647d053cd77f5e93c474
Jullianos <harlusak85@gmail.com>
2022-08-11 18:42:18 +0200

    Create a schedule logs service

---
+++ app/services/schedule_logs.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'travis/lock'
+require 'redlock'
+
+class ScheduleLogs
+  def initialize(from, to)
+    @from = from
+    @to = to
+  end
+
+  def call
+    Rails.logger.info("Scheduling logs from=[#{@from}] to=[#{@to}]")
+
+    Log.where.not(:scan_status => [:queued, :ready_for_scan])
+       .where(created_at: @from..@to)
+       .update_all(scan_status: :ready_for_scan, scan_status_updated_at: Time.now)
+  end
+end
