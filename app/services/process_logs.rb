cd2ce8fe0c115cfc01967d2116a8142b27026269
Jullianos <harlusak85@gmail.com>
2022-09-18 15:07:09 +0200

    Implement exception handling on log level and refactor code

--- app/services/process_logs.rb
+++ app/services/process_logs.rb
@@ -13,10 +13,11 @@ class ProcessLogs
 
     begin
       Travis::Lock.exclusive('process_logs', lock_options) do
-        logs = Log.where(id: @log_ids, scan_status: :queued)
+        logs_query = Log.where(id: @log_ids, scan_status: :queued)
+        logs = logs_query.to_a
         queued_log_ids = logs.map { |log| { log_id: log.id } }
         ApplicationRecord.transaction do
-          logs.update_all(scan_status: :started, scan_status_updated_at: Time.now)
+          logs_query.update_all(scan_status: :started, scan_status_updated_at: Time.now)
           ScanTrackerEntry.create(queued_log_ids) do |entry|
             entry.scan_status = :started
           end
@@ -32,9 +33,9 @@ class ProcessLogs
 
   def process_logs(logs)
     unless logs.empty?
-      begin
-        logs.each do |log|
-          Rails.logger.info("Processing log with id=[#{log.id}] and content=[#{log.content}]")
+      logs.each do |log|
+        begin
+          Rails.logger.info("Processing log with id=[#{log.id}]")
           remote_log = Travis::RemoteLog.new(:aggregated_at => log.aggregated_at,
                                              :archive_verified => log.archive_verified,
                                              :archived_at => log.archived_at,
@@ -45,7 +46,7 @@ class ProcessLogs
                                              :job_id => log.job_id,
                                              :purged_at => log.purged_at,
                                              :removed_at => log.removed_at,
-                                             :removed_by_id => log.removed_by_id,
+                                             :removed_by_id => log.removed_by,
                                              :updated_at => log.updated_at)
           if remote_log.archived?
             remote_log.archived_log_content
@@ -53,16 +54,24 @@ class ProcessLogs
           else
             write_log_to_file(remote_log.id, remote_log.job_id, remote_log.content)
           end
+        rescue Exception => e
+          ApplicationRecord.transaction do
+            log.scan_status = :error
+            log.scan_status_updated_at = Time.now
+            log.save
+
+            ScanTrackerEntry.create(:log_id => log.id, :scan_status => :error)
+          end
+
+          Sentry.capture_exception(e)
+          Rails.logger.error(e.message)
         end
-      rescue Exception => e
-        Sentry.catch_exception(e)
-        Rails.logger.error(e.message)
       end
     end
   end
 
   def write_log_to_file(id, job_id, content)
-    File.open(File.join(__dir__, "../../log/#{id}-#{job_id}")) do |file|
+    File.open("/srv/app/log/#{id}-#{job_id}.log", "w") do |file|
       file.write(content)
     end
   end
