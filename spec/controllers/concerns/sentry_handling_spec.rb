02abb21f395dab728c4e8bc55870ca01ab7c07e7
AndriiMysko <usandrewmisko@gmail.com>
2022-10-12 13:14:45 +0300

    Rubocop

--- spec/controllers/concerns/sentry_handling_spec.rb
+++ spec/controllers/concerns/sentry_handling_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe SentryHandling, type: :controller do
   controller(ApplicationController) do
-    include SentryHandling
+    include SentryHandling # rubocop:disable RSpec/DescribedClass
 
-    def fake_action; redirect_to '/an_url'; end
+    def fake_action = redirect_to('/an_url')
   end
 
   before do
-    routes.draw {
+    routes.draw do
       get 'fake_action' => 'anonymous#fake_action'
-    }
+    end
   end
-    
+
   describe 'sentry_handling' do
     before do
       allow(Sentry).to receive(:set_extras)
