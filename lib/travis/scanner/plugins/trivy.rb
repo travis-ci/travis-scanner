b11342f2ca459edb6546fa5af3a3be9e8c38e7ad
Gabriel Rat <rat.gabriel@outlook.com>
2022-10-15 14:39:37 +0000

    Fix for RSA keys censoring

--- lib/travis/scanner/plugins/trivy.rb
+++ lib/travis/scanner/plugins/trivy.rb
@@ -9,6 +9,60 @@ module Travis
           @json_data = ''
         end
 
+        private
+
+        def get_log_newline_locations(log_id)
+          file_path = File.join(@current_run_logs_path, log_id)
+          newline_locations = [-1]
+          puts file_path
+          File.readlines(file_path).each do |line|
+              newline_locations << line.length + newline_locations[-1]
+          end
+          newline_locations.drop(1)
+        end
+
+        def compute_endings(finding)
+          newline_locations = get_log_newline_locations finding[:log_id]
+          final_scan_findings = []
+          lines_to_add = 0
+          finding[:scan_findings] = finding[:scan_findings].sort_by {|obj| obj[:start_line]}
+          (0..finding[:scan_findings].length-1).each do |index|
+            scan_finding = finding[:scan_findings][index]
+            actual_line = scan_finding[:start_line] + lines_to_add - 2
+            if index > 0
+              scan_finding_before = finding[:scan_findings][index - 1]
+              if actual_line < scan_finding_before[:end_line]
+                actual_line = scan_finding_before[:end_line] - 1
+                scan_finding[:start_column] -= newline_locations[scan_finding_before[:end_line] - 1] - newline_locations[scan_finding_before[:start_line]-2]
+              end
+            end
+            line_size = newline_locations[actual_line + 1] - newline_locations[actual_line]
+            line_size -= (scan_finding[:start_column] - 1)
+            end_column = scan_finding[:size] + 1
+            newlines_found = 0
+            while line_size < scan_finding[:size]
+              end_column = scan_finding[:size] - line_size
+              newlines_found += 1
+              line_size += (newline_locations[actual_line + newlines_found + 1] - newline_locations[actual_line + newlines_found])
+            end
+            lines_to_add += newlines_found
+            if newlines_found > 0
+              if line_size == scan_finding[:size]
+                lines_to_add += 1
+              end
+            else
+              end_column += (scan_finding[:start_column] - 1)
+            end
+            scan_finding[:start_line] = actual_line + 2
+            scan_finding[:start_column] = scan_finding[:start_column]
+            scan_finding[:end_line] = scan_finding[:start_line] + newlines_found
+            scan_finding[:end_column] = end_column
+            scan_finding.delete(:column)
+            scan_finding.delete(:line)
+          end
+          finding
+        end
+
         protected
 
         def compute_command_line(logs_path)
@@ -26,7 +80,6 @@ module Travis
           results = []
           scan_result['Results'].each do |result|
             next unless result['Class'] == 'secret' || result['Secrets'].empty?
-
             results << process_result(result)
           end
 
@@ -43,22 +96,22 @@ module Travis
             log_id: result['Target'],
             scan_findings: []
           }
-          result['Secrets'].each do |secret|
-            match_data = secret['Match'].to_enum(:scan, /\*+/).map do
-              [Regexp.last_match.begin(0) + 1, Regexp.last_match.to_s.length]
-            end
 
+          scan_findings = {}
+          result['Secrets'].each do |secret|
+            match_data = secret['Code']['Lines'][2]['Content'].to_enum(:scan, /\*+/).map { [Regexp.last_match.begin(0) + 1, Regexp.last_match.to_s.length] }
+            
             match_data.each do |match|
-              finding[:scan_findings] << {
+              scan_findings[[secret['Title'], secret['StartLine'], match.first, match.last]] = {
                 name: secret['Title'],
-                line: secret['StartLine'],
-                column: match.first,
-                size: match.last
+                start_line: secret['StartLine'],
+                start_column: match.first,
+                size: match.last,
               }
             end
           end
-
-          finding
+          finding[:scan_findings] = scan_findings.values
+          compute_endings(finding)
         end
       end
     end
--- lib/travis/scanner/runner.rb
+++ lib/travis/scanner/runner.rb
@@ -25,12 +25,14 @@ module Travis
             log_id = result[:log_id]
             logs[log_id] ||= {}
             result[:scan_findings].each do |scan_finding|
-              (logs[log_id][scan_finding[:line]] ||= []).push({
-                                                                plugin_name: scanner_result[:scanner_name],
-                                                                finding_name: scan_finding[:name],
-                                                                column: scan_finding[:column],
-                                                                size: scan_finding[:size]
-                                                              })
+              new_finding = {
+                plugin_name: scanner_result[:scanner_name],
+                finding_name: scan_finding[:name],
+              }
+              new_finding[:start_column] = scan_finding[:start_column] if scan_finding.key?(:start_column)
+              new_finding[:end_column] = scan_finding[:end_column] if scan_finding.key?(:end_column)
+              new_finding[:end_line] = scan_finding[:end_line] if scan_finding.key?(:end_line)
+              (logs[log_id][scan_finding[:start_line]] ||= []).push(new_finding)
             end
           end
         end
