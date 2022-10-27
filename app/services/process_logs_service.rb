8526712aec16ebd0a5aa8a84e77f824197704cdc
Gabriel Rat <rat.gabriel@outlook.com>
2022-10-27 14:36:31 +0000

    Replacing Windows and Mac endline characters before scanning

--- app/services/process_logs_service.rb
+++ app/services/process_logs_service.rb
@@ -34,28 +34,77 @@ class ProcessLogsService < BaseLogsService
   end
 
   def process_logs(logs)
-    logs.each { |log| download_log(log) }
+    substitutions_for_log = {}
+    logs.each do |log|
+      content = download_log(log)
+      next if content.blank?
+
+      substitutions_for_log[log.id] = []
+      content = substitute_regexes(content, ["\r\n", "\r"], "\n", substitutions_for_log[log.id])
+      write_log_to_file(log.id, content)
+    end
 
     run_scanner(logs)
+
+    logs.each do |log|
+      content = read_log_from_file("#{log.id}_censored")
+      content = revert_substitutions(content, substitutions_for_log[log.id].reverse)
+      write_log_to_file("#{log.id}_censored", content)
+    end
+  end
+
+  def substitute_regexes(content, regs_to_replace, to_str, substitutions)
+    regs_to_replace.each do |from_reg|
+      substitutions_from_reg = []
+      content = replace_in_content(content, from_reg, to_str, substitutions_from_reg)
+      substitutions.push([from_reg, substitutions_from_reg])
+    end
+    content
+  end
+
+  def revert_substitutions(content, substitutions)
+    substitutions.each do |reg, substitutions_of_reg|
+      substitutions_of_reg.each do |index|
+        if reg.length == 1
+          content[index] = reg
+        else
+          before = content[..index - 1]
+          after = (index + 1) < content.length ? content[index + 1..] : ''
+          content = before + reg + after
+        end
+      end
+    end
+    content
+  end
+
+  def replace_in_content(content, from_reg, to_str, substitutions_of_str)
+    r_to_replace = Regexp.new(from_reg)
+    content.enum_for(:scan, r_to_replace).map do
+      substitutions_of_str.push(Regexp.last_match.offset(0).first)
+    end
+    content.gsub(r_to_replace, to_str)
   end
 
   def download_log(log)
     Rails.logger.info("Processing log with id=[#{log.id}]")
 
     remote_log = Travis::RemoteLog.new(log.job_id, log.archived_at, log.archive_verified?)
-    write_log_to_file(
-      log.id,
-      remote_log.archived? ? remote_log.archived_log_content : log.content
-    )
+
+    remote_log.archived? ? remote_log.archived_log_content : log.content
   rescue => e
     update_logs_status([log.id], :error)
 
     Rails.logger.error(e.message)
     Sentry.capture_exception(e)
+    nil
   end
 
   def write_log_to_file(id, content)
-    File.write(File.join(build_job_logs_dir, "#{id}.log"), content) if content.present?
+    File.write(File.join(build_job_logs_dir, "#{id}.log"), content)
+  end
+
+  def read_log_from_file(file_name)
+    File.read(File.join(build_job_logs_dir, "#{file_name}.log"))
   end
 
   def build_job_logs_dir
